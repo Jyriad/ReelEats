48905dc53738e6ef7ae9a125e864c3f9280ade31
Jyriad <joshrice112@gmail.com>
2025-10-30 08:36:25 +0000

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // This file contains all shared configuration constants for the ReelEats application
 
 // Centralized version number - update this single variable to change version everywhere
-export const versionNumber = '1.831';
+export const versionNumber = '1.832';
 
 export const CONFIG = {
     // Supabase Configuration
